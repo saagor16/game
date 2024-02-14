06422c7dd284f88b0544c78872700e32f558de63
saagor16 <saagor.16@gmail.com>
2024-02-14 20:07:54 +0600

    first module3

--- script/alpha.js
+++ script/alpha.js
@@ -4,6 +4,23 @@
 //     const playgroundSection = document.getElementById('play-ground');
 //     playgroundSection.classList.remove('hidden');
 // }
+function handleKeyboardButtonPress(event){
+    const playerPressed = event.key;
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet =currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    if(playerPressed === expectedAlphabet){
+        console.log('get a point');
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    }
+    else{
+        console.log('you missed a life');
+    }
+}
+document.addEventListener('keyup', handleKeyboardButtonPress)
+
+
 function continueGame(){
     const alphabet =getARandomAlphabet();
     const currentAlphabetElement = document.getElementById('current-alphabet');
